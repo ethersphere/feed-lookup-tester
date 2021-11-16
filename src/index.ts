f98996ac49389078af7172d7a2df4774b7113e37
nugaon <toth.viktor.levente@gmail.com>
2021-11-16 16:33:10 +0100

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,132 @@
+#!/usr/bin/env node
+
+import { Bee, Utils } from '@ethersphere/bee-js'
+import { ChunkReference } from '@ethersphere/bee-js/dist/src/feed'
+import { FetchFeedUpdateResponse } from '@ethersphere/bee-js/dist/src/modules/feed'
+import ora from 'ora'
+import yargs from 'yargs'
+import { feedIndexBeeResponse, incrementBytes, makeBytes, randomByteArray } from './utils'
+
+const zeros64 = '0000000000000000000000000000000000000000000000000000000000000000'
+const syncPollingTime = 1000 //in ms
+const syncPollingTrials = 15
+
+export const testIdentity = {
+  privateKey: '634fb5a872396d9693e5c9f9d7233cfa93f395c093371017ff44aa9ae6564cdd',
+  publicKey: '03c32bb011339667a487b6c1c35061f15f7edc36aa9a0f8648aba07a4b8bd741b4',
+  address: '8d3766440f0d7b949a5e32995d09619a7f86e632',
+}
+
+function fetchDataCheck(updateFetch: FetchFeedUpdateResponse, expectedFeedRef: ChunkReference, expectedFeedIndex: number) {
+  const beeFeedIndex = feedIndexBeeResponse(expectedFeedIndex)
+  const feedRef =  Utils.bytesToHex(expectedFeedRef)
+
+  if(updateFetch.feedIndex !== beeFeedIndex || feedRef !== updateFetch.reference) {
+    throw Error(`Downloaded feed payload or index has not the expected result.`
+      + `\n\tindex| expected: "${beeFeedIndex}" got: "${updateFetch.feedIndex}"`
+      + `\n\treference| expected: "${feedRef}" got: "${updateFetch.reference}"`)
+  }
+}
+
+async function waitSyncing(bee: Bee, tagUid: number): Promise<void | never> {
+  const pollingTime = syncPollingTime
+  const pollingTrials = syncPollingTrials
+  let synced = false
+  let syncStatus = 0
+
+  for (let i = 0; i < pollingTrials; i++) {
+    const tag = await bee.retrieveTag(tagUid)
+
+    if (syncStatus !== tag.synced) {
+      i = 0
+      syncStatus = tag.synced
+    }
+
+    if (syncStatus >= tag.total) {
+      synced = true
+      break
+    }
+  }
+
+  if (!synced) {
+    throw new Error('Data syncing timeout.')
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-extra-semi
+;(async function root() {
+  const argv = await yargs(process.argv.slice(2))
+    .usage('Usage: <some STDOUT producing command> | $0 [options]')
+    .option('bee-writer', {
+      alias: 'b1',
+      type: 'string',
+      describe: 'Writer Bee node URL. By default Gateway 9 is used.',
+      default: 'https://bee-9.gateway.ethswarm.org'
+    })
+    .option('bee-reader', {
+      alias: 'b2',
+      type: 'string',
+      describe: 'Reader Bee node URL. By default Gateway 8 is used.',
+      default: 'https://bee-8.gateway.ethswarm.org'
+    })
+    .option('stamp', {
+      alias: 'st',
+      type: 'string',
+      describe: 'Postage Batch Stamp ID for bee-writer. By default it is zeros',
+      default: zeros64
+    })
+    .option('updates', {
+      alias: 'x',
+      type: 'number',
+      describe: 'How many updates the script will do',
+      default: 2
+    })
+    .option('topic-seed', {
+      alias: 't',
+      type: 'number',
+      describe: 'From what seed the random topic will be generated',
+      default: 10
+    })
+    .help('h')
+    .alias('h', 'help').epilog(`Testing Ethereum Swarm Feed lookup time`).argv
+
+  const beeWriterUrl = process.env.BEE_API_URL || argv['bee-writer']
+  const beeReaderUrl = process.env.BEE_PEER_API_URL || argv['bee-reader']
+  const stamp = process.env.BEE_STAMP || argv.stamp
+  const updates = argv.updates
+  const topicSeed = argv['topic-seed']
+
+  const beeWriter = new Bee(beeWriterUrl)
+  const beeReader = new Bee(beeReaderUrl)
+
+  const topic = randomByteArray(32, topicSeed)
+  const feedWriter = beeWriter.makeFeedWriter('sequence', topic, testIdentity.privateKey)
+  const feedReader = beeReader.makeFeedReader('sequence', topic, testIdentity.address)
+
+  // reference that the feed refers to
+  const reference = makeBytes(32) // all zeroes
+
+  for(let i = 0; i < updates; i++) {
+    const spinner = ora(`Upload feed for index ${i}`)
+    spinner.start()
+    
+    // create tag for the full sync
+    spinner.text = `Wait for feed update sync at index ${i}`
+    const tag = await beeWriter.createTag()
+    await feedWriter.upload(stamp, reference, { tag: tag.uid })
+    await waitSyncing(beeWriter, tag.uid)
+
+    spinner.text = `Download feed for index ${i}`
+
+    // timer start
+    const firstUpdateFetch = await feedReader.download()
+    // timer stop
+
+    fetchDataCheck(firstUpdateFetch, reference, i)
+
+    spinner.text = `Feed update ${i} fetch was successful`
+    spinner.stopAndPersist()
+
+    incrementBytes(reference)
+  }
+})()
---
+++ src/utils.ts
@@ -0,0 +1,52 @@
+import { Bytes } from "@ethersphere/bee-js/dist/src/utils/bytes"
+
+/**
+ * Utility function for generating random Buffer
+ * !!! IT IS NOT CRYPTO SAFE !!!
+ * For that use `crypto.randomBytes()`
+ *
+ * @param length Number of bytes to generate
+ * @param seed Seed for the pseudo-random generator
+ */
+ export function randomByteArray(length: number, seed = 500): Uint8Array {
+  const rand = lrng(seed)
+  const buf = new Uint8Array(length)
+
+  for (let i = 0; i < length; ++i) {
+    buf[i] = (rand() * 0xff) << 0
+  }
+
+  return buf
+}
+
+/**
+ * Lehmer random number generator with seed (minstd_rand in C++11)
+ * !!! Very fast but not well distributed pseudo-random function !!!
+ *
+ * @param seed Seed for the pseudo-random generator
+ */
+ function lrng(seed: number): () => number {
+  return (): number => ((2 ** 31 - 1) & (seed = Math.imul(48271, seed))) / 2 ** 31
+}
+
+/**
+ * Returns a new byte array filled with zeroes with the specified length
+ *
+ * @param length The length of data to be returned
+ */
+ export function makeBytes<Length extends number>(length: Length): Bytes<Length> {
+  return new Uint8Array(length) as Bytes<Length>
+}
+
+export function feedIndexBeeResponse(index: number): string {
+  return String(index).padStart(16, '0')
+}
+
+export function incrementBytes(bytes: Uint8Array): void {
+  for(let i = bytes.length - 1; i >= 0; i--) {
+    if(bytes[i] < 255) {
+      bytes[i]++
+      break
+    }
+  }
+}
